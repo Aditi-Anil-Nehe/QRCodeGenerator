144cc8480fc68f8b70259189c5bb04969c355c62
Aditi Nehe <aditianilnehe22@gmail.com>
2025-09-05 13:00:54 +0530

    changes commited

--- index.js
+++ index.js
@@ -1,10 +1,30 @@
 const inquirer = require("inquirer");
+const fs = require("fs");
+const path = require("path");
+const QRCode = require("qrcode");
+const { Console } = require("console");
+
+const uploadDir = path.join(__dirname,"uploads");
+if(!fs.existsSync(uploadDir)) fs.mkdirSync(uploadDir);
+
+async function generateQRCode(text) {
+   const fileName = `qrcode_${Date.now()}.png`;
+   const filePath = path.join(uploadDir,fileName);
+   await QRCode.toFile(filePath,text);
+   return filePath;
+}
 
 async function main(){
-   const{text} = await inquirer.createPromptModule([
+   const{text} = await inquirer.prompt([
     {type : "input", name:"text",message:"Enter a string/URL to generate QRcode:"}
    ]);
-   console.log("you entered:",text);
+    
+   if(!text.trim()){
+      console.log("No text Provided. Exiting.")
+      return;
+   }
+   const filePath = await generateQRCode(text.trim());
+   console.log("QR code saved at:", filePath);
 }
 
 main();
